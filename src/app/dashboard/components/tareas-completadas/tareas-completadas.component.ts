af467c976e24ab91113a6437a312984a767a10a3
Sergio Sanz <sergiosanz2004@gmail.com>
2024-05-11 19:13:07 +0200

    tareas pendienes y completadas

--- src/app/dashboard/components/tareas-completadas/tareas-completadas.component.ts
+++ src/app/dashboard/components/tareas-completadas/tareas-completadas.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TareasAsignadasComponent } from '../tareas-asignadas/tareas-asignadas.component';
 
 @Component({
   selector: 'app-tareas-completadas',
   templateUrl: './tareas-completadas.component.html',
   styleUrl: './tareas-completadas.component.css'
 })
-export class TareasCompletadasComponent {
+export class TareasCompletadasComponent extends TareasAsignadasComponent implements OnInit{
 
+  override ngOnInit() {
+    super.ngOnInit();
+  }
+
+  get completedTasks() {
+    return this.tasksList.filter(task => task.status);
+  }
 }
--- src/app/dashboard/components/tareas-pendientes/tareas-pendientes.component.ts
+++ src/app/dashboard/components/tareas-pendientes/tareas-pendientes.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TareasAsignadasComponent } from '../tareas-asignadas/tareas-asignadas.component';
 
 @Component({
   selector: 'app-tareas-pendientes',
   templateUrl: './tareas-pendientes.component.html',
   styleUrl: './tareas-pendientes.component.css'
 })
-export class TareasPendientesComponent {
+export class TareasPendientesComponent extends  TareasAsignadasComponent implements OnInit {
 
+  override ngOnInit() {
+    super.ngOnInit();
+  }
+
+  get pendingTasks() {
+    return this.tasksList.filter(task => !task.status);
+  }
 }
