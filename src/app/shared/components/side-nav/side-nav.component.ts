208b4f9cfa27c81182bedebaa36b81706c30021f
Sergio Sanz <sergiosanz2004@gmail.com>
2024-05-12 17:00:34 +0200

    modo oscuro

--- src/app/shared/components/side-nav/side-nav.component.ts
+++ src/app/shared/components/side-nav/side-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
@@ -6,11 +6,44 @@ import { AuthService } from '../../../auth/services/auth.service';
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.css'
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit {
+
   private authService = inject(AuthService);
 
+  ngOnInit(): void {
+    const theme = localStorage.getItem('theme');
+    const body = document.body;
+    const toggle = document.getElementById('toggle') as HTMLInputElement;
+    if (theme) {
+      body.classList.add(theme);
+      if (theme === 'dark-mode') {
+        toggle.checked = true;
+      } else {
+        toggle.checked = false;
+      }
+    } else {
+      body.classList.add('light-mode');
+      toggle.checked = false;
+    }
+  }
 
-  onLogout(){
+  onLogout() {
     this.authService.logout()
+  }
+
+  toggleTheme() {
+    const body = document.body;
+    const toggle = document.getElementById('toggle') as HTMLInputElement;
+    if (body.classList.contains('dark-mode')) {
+      body.classList.remove('dark-mode');
+      body.classList.add('light-mode');
+      localStorage.setItem('theme', 'light-mode');
+      toggle.checked = false;
+    } else {
+      body.classList.remove('light-mode');
+      body.classList.add('dark-mode');
+      localStorage.setItem('theme', 'dark-mode');
+      toggle.checked = true;
+    }
   }
 }
