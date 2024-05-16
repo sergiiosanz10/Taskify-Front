6c3e5a671fd3f963fe8ca27ddec56968af6609aa
Sergio Sanz García <sergiosanz2004@gmail.com>
2024-05-16 10:42:45 +0200

    todo signals

--- src/app/dashboard/components/tasks-list/tareas-asignadas.component.ts
+++ src/app/dashboard/components/tasks-list/tareas-asignadas.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { DashboardService } from '../../services/dashboard.service';
 import { TaskResponse } from '../../interfaces/task.interface';
 import { NavigationStart, Router } from '@angular/router';
-import { filter } from 'rxjs';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-tareas-asignadas',
@@ -14,13 +14,14 @@ export class TareasAsignadasComponent implements OnInit {
   private activeRoute = inject(Router)
   private DashboardService = inject(DashboardService);
 
-  public groupedTasks: Map<string, TaskResponse[]> | undefined
-  public tasksList: TaskResponse[] = [];
-  public uniqueColors: string[] = [];
-  public uniqueLabels: string[] = [];
-  public filterParam: string = "";
-  public type: string = "";
-  public listDate: string[] = [];
+  // public groupedTasks: Map<string, TaskResponse[]> | undefined
+  public groupedTasks= signal<Map<string, TaskResponse[]> | undefined>(undefined)
+  public tasksList = signal<TaskResponse[]>([]);
+  public uniqueColors = signal<string[]>([]);
+  public uniqueLabels = signal<string[]>([]);
+  public filterParam = signal<string>('');
+  public type = signal<string>('');
+  public listDate = signal<string[]>([]);
 
 
   ngOnInit() {
@@ -29,16 +30,16 @@ export class TareasAsignadasComponent implements OnInit {
       filter((event) => event instanceof NavigationStart)
     ).subscribe((event: any) => {
       let params: string = event["url"].split("/")
-      this.type = params[2]
-      if (this.type == "all" || this.type == "pending" || this.type == "complete") {
-        this.filterParam = ""
+      this.type.set(params[2])
+      if (this.type() == "all" || this.type() == "pending" || this.type() == "complete") {
+        this.filterParam.set('')
         this.loadTasks()
       }
-      this.tasksList = []
-      this.listDate = []
+      this.tasksList.set([])
+      this.listDate.set([])
     })
 
-    if (this.type == "") {
+    if (this.type() == "") {
       this.loadTasks()
     }
   }
@@ -47,51 +48,51 @@ export class TareasAsignadasComponent implements OnInit {
   loadTasks() {
     const token = sessionStorage.getItem('token');
     if (!token) return;
-    this.groupedTasks = new Map();
+    this.groupedTasks.set(new Map());
     this.DashboardService.getTasks(token)
       .subscribe(tasks => {
-        this.tasksList = tasks;
+        this.tasksList.set(tasks)
         this.groupTasksByDate();
         this.sortTasks();
-        this.uniqueColors = [...new Set(this.tasksList.map(task => task.color))];
-        this.uniqueLabels = [...new Set(this.tasksList.map(task => task.label))];
+        this.uniqueColors.set([...new Set(this.tasksList().map(task => task.color))]);
+        this.uniqueLabels.set([...new Set(this.tasksList().map(task => task.label))]);
 
       });
   }
 
   groupTasksByDate() {
     //Limpio el Map
-    this.groupedTasks = new Map();
+    this.groupedTasks.set(new Map());
 
     //Limpio la lista de fechas
-    this.listDate = []
+    this.listDate.set([])
 
-    this.tasksList.forEach(task => {
+    this.tasksList().forEach(task => {
       const date = task.date || '';
-      if (!this.groupedTasks?.has(date)) {
+      if (!this.groupedTasks()?.has(date)) {
         var list = this.getTaskListInTheDay(date)
-        this.listDate.push(date)
-        this.groupedTasks?.set(date, list);
+        this.listDate().push(date)
+        this.groupedTasks()?.set(date, list);
       }
     })
   }
 
   getTaskListInTheDay(date: string) {
 
-    var list = this.tasksList.filter(task => {
+    var list = this.tasksList().filter(task => {
 
-      if ((this.type == "all" || this.type == "" || this.type == undefined) && task.date === date) {
+      if ((this.type() == "all" || this.type() == "" || this.type == undefined) && task.date === date) {
         return true;
-      } else if (this.type == "pending" && task.date === date && task.status === false) {
+      } else if (this.type() == "pending" && task.date === date && task.status === false) {
         return true;
-      } else if (this.type == "complete" && task.date == date && task.status === true) {
+      } else if (this.type() == "complete" && task.date == date && task.status === true) {
         return true;
       }
       return false;
     });
 
-    if (this.filterParam != "") {
-      list = list.filter(task => task.label === this.filterParam)
+    if (this.filterParam() != "") {
+      list = list.filter(task => task.label === this.filterParam())
     }
     return list
   }
@@ -103,7 +104,7 @@ export class TareasAsignadasComponent implements OnInit {
 
     this.DashboardService.deleteTask(id, token)
       .subscribe(() => {
-        this.tasksList = this.tasksList.filter(task => task.taskId !== id);
+        this.tasksList.set(this.tasksList().filter(task => task.taskId !== id))
         this.groupTasksByDate();
         this.sortTasks();
       });
@@ -116,25 +117,25 @@ export class TareasAsignadasComponent implements OnInit {
 
     this.DashboardService.modifyTask(token, task)
       .subscribe(updatedTask => {
-        const index = this.tasksList.findIndex(t => t.taskId === updatedTask.taskId);
+        const index = this.tasksList().findIndex(t => t.taskId === updatedTask.taskId);
         if (index !== -1) {
-          this.tasksList[index] = updatedTask;
+          this.tasksList()[index] = updatedTask;
           this.groupTasksByDate();
           this.sortTasks();
-          this.uniqueColors = [...new Set(this.tasksList.map(task => task.color))];
-          this.uniqueLabels = [...new Set(this.tasksList.map(task => task.label))];
+          this.uniqueColors.set([...new Set(this.tasksList().map(task => task.color))]);
+          this.uniqueLabels.set([...new Set(this.tasksList().map(task => task.label))]);
         }
       });
   }
 
 
   sortTasks() {
-    this.tasksList.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+    this.tasksList().sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
   }
 
   filterByLabel(label: string) {
-    this.filterParam = label
-    this.groupedTasks = new Map()
+    this.filterParam.set(label)
+    this.groupedTasks.set(new Map());
     this.loadTasks()
   }
 
