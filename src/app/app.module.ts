d06f7ae5ac2c64e76e20c182a912f6cec2fc45d9
Sergio Sanz <sergiosanz2004@gmail.com>
2024-05-22 19:54:50 +0200

    loader checking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { JwtInterceptor } from './shared/services/interceptors/jwt-interceptor';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    SharedModule
   ],
   providers: [
     provideAnimationsAsync(),
